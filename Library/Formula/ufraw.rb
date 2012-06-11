8ec00614e066eb80656e381d59e13046ea6c2f71
Devin Weaver <suki@tritarget.org>
2012-06-11 08:56:29 -0400

    Updates Formula: ufraw
    UFRaw requires GTK to build the graphical portion. Using the
    `--without-gtk` option to configure will onluy build a batch command
    line. This is the default behavior for this formula.
    
    This commit adds an option to the formula so the user can choose to
    include the GTK dependancy and build the graphical front end for UFRaw.

--- Library/Formula/ufraw.rb
+++ Library/Formula/ufraw.rb
@@ -7,18 +7,30 @@ class Ufraw <Formula
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
+  depends_on 'gtk' if ARGV.include? '--with-gtk'
   depends_on 'libtiff'
   depends_on 'jpeg'
   depends_on 'little-cms'
   depends_on 'dcraw'
   depends_on 'exiv2' => :optional
 
+  def options
+    [
+      ['--with-gtk', 'Build the UFRaw GUI (depends on GTK)'],
+    ]
+  end
+
   def install
     fails_with_llvm "Compiling with LLVM gives a segfault while linking."
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--without-gtk",
-                          "--without-gimp"
+    config_opts = [
+      "--disable-dependency-tracking",
+      "--prefix=#{prefix}",
+      "--without-gimp",
+    ]
+    config_opts << "--without-gtk" unless ARGV.include? "--with-gtk"
+
+    system "./configure", config_opts
+
     system "make install"
     (share+'pixmaps').rmtree
   end
